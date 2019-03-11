c64c1bbbb3245ba64036b3d8d22c066c287465c1
Jan Sechovec <jsechovec@csas.cz>
2019-03-11 16:42:42 +0100

    data fetch logic extracted into hook :hammer:

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -1,41 +1,41 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import axios from 'axios';
+import { useHackerNewsApi } from '../hooks/useHackerNewsApi';
 
 const  Articles = () => {
-  const [data, setData] = useState({ hits: [] });
   const [query, setQuery] = useState('redux');
-  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=redux');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(url);
-      setData(result.data);
-    };
-    fetchData();
-  }, [url]);
+  const {data, isLoading, isError, doFetch} = useHackerNewsApi();
 
   return (
     <Fragment>
-      <input
-        type="text"
-        value={query}
-        onChange={event => setQuery(event.target.value)}
-      />
-      <button
-        type="button"
-        onClick={() =>
-          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
-        }
-      >
-        Search
-      </button>
-      <ul>
-        {data.hits.map(item => (
-          <li key={item.objectID}>
-            <a href={item.url}>{item.title}</a>
-          </li>
-        ))}
-      </ul>
+        <form onSubmit={event => {
+          doFetch(
+            `http://hn.algolia.com/api/v1/search?query=${query}`,
+          );
+          event.preventDefault();
+        }}>
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading...</div>
+      ): (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      
     </Fragment>
   );
 }
